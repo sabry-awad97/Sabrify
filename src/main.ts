4adcf52264cd9d4b9478549bbacc3e0c8abaf4d0
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-15 00:53:48 +0200

    Define collapsibles

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
 import 'normalize.css';
 import './css/style.scss';
+
+const collapsibles = document.querySelectorAll<HTMLDivElement>('.collapsible');
+
+for (const item of Array.from(collapsibles)) {
+  item.addEventListener('click', function () {
+    this.classList.toggle('collapsible--expanded');
+  });
+}
