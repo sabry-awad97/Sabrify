cdf870550e78c9aeb32325674a6c7a76008d9c51
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-15 05:05:00 +0200

    implementing animation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import 'normalize.css';
 import './css/style.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const collapsibles = document.querySelectorAll<HTMLDivElement>('.collapsible');
 
